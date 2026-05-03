27561c09b606d437256aeb4e0199b3d9798e9dca
Dkjawels83 <mkjawels83@gmail.com>
2026-05-03 22:50:53 +0530

    stop instance

--- backend/services/aws_ec2.py
+++ backend/services/aws_ec2.py
@@ -38,3 +38,32 @@ def start_instance(instance_id: str):
 
     except Exception as e:
         return {"error": str(e)}
+
+def stop_instance(instance_id: str):
+    try:
+        response = ec2_client.describe_instances(InstanceIds=[instance_id])
+    
+
+        reservations = response.get("Reservations")
+
+        if not reservations:
+            return {"error": "Instance not found"}
+
+        instance = reservations[0]["Instances"][0]
+        state = instance["State"]["Name"]
+#hia kuch
+        # If already running
+        if state == "stopped":
+            return {"message": "Instance already running"}
+
+        # If already starting
+        if state == "pending":
+            return {"message": "Instance is already starting"}
+
+        # Start instance
+        ec2_client.start_instances(InstanceIds=[instance_id])
+
+        return {"message": "Instance starting"}
+
+    except Exception as e:
+        return {"error": str(e)}
